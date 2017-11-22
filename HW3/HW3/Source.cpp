1e1f48110f2dfbe202aa25f4c13b70104390aa16
tzafrirhn <tzafrirhn@gmail.com>
2017-11-22 17:00:37 +0200

    Update Source.cpp

--- HW3/HW3/Source.cpp
+++ HW3/HW3/Source.cpp
@@ -4,5 +4,6 @@ using namespace std;
 void main()
 {
 	cout << "H e l l o    W o r l d"<< endl << "welcome user 235"<< endl;
+	cout << endl << "thank you for using our system..GoodBye" << endl;
 	system("pause");
 }
